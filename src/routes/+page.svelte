08d4e6944b74bcea4aedadfa4b1063ef41340c69
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-12 11:04:10 -0400

    Add stylish paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,13 @@
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+<p>This is a paragraph.</p>
 <img src={src} alt="A man dances." />
+
+<style>
+    p {
+        color: goldenrod;
+        font-family:'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
+</style>
