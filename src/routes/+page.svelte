35901f0095fb677a183433411b1c591746c21042
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-12 11:24:06 -0400

    Read HTML in a string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 
     let name = 'Svelte';
     let src = './rickroll.gif';
+    let string = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
 <p>This is a paragraph.</p>
 <Nested />
+<p>{@html string}</p>
 <img src={src} alt="A man dances." />
 
 <style>
