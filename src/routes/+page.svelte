7820f740a0e1b1ba3c502ff438bc97bfe4d34420
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-12 11:18:05 -0400

    Create Nested component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
+    import Nested from "./Nested.svelte";
+
     let name = 'Svelte';
     let src = './rickroll.gif';
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
 <p>This is a paragraph.</p>
+<Nested />
 <img src={src} alt="A man dances." />
 
 <style>
