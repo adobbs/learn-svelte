b745b7bd09701966ec2fde0d812a36779e96e344
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-21 11:26:35 -0400

    Keep each mapping in sync using id

---
+++ src/components/Thing.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	interface Emojis {
+        apple: string;
+        banana: string;
+        carrot: string;
+        doughnut: string;
+        egg: string;
+    }
+
+    const emojis: Emojis = {
+		apple: '🍎',
+		banana: '🍌',
+		carrot: '🥕',
+		doughnut: '🍩',
+		egg: '🥚'
+	};
+
+	// the name is updated whenever the prop value changes...
+	export let name: keyof Emojis;
+
+	// ...but the "emoji" variable is fixed upon initialisation
+	// of the component because it uses `const` instead of `$:`
+	const emoji = emojis[name];
+</script>
+
+<p>{emoji} = {name}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
-<script>
+<script lang="ts">
     import Each from "../components/Each.svelte";
     import Nested from "../components/Nested.svelte";
     import PackageInfo from '../components/PackageInfo.svelte';
+
+    import Thing from '../components/Thing.svelte';
+
+    let things = [
+        { id: 1, name: 'apple' },
+        { id: 2, name: 'banana' },
+        { id: 3, name: 'carrot' },
+        { id: 4, name: 'doughnut' },
+        { id: 5, name: 'egg' }
+    ];
+
+    function handleClick() {
+        things = things.slice(1);
+    }
 
 	const pkg = {
 		name: 'svelte',
@@ -35,6 +49,14 @@
 
 	$: sum = numbers.reduce((t, n) => t + n, 0);
 </script>
+
+<button on:click={handleClick}>
+	Remove first thing
+</button>
+
+{#each things as thing (thing.id)}
+	<Thing name={thing.name} />
+{/each}
 
 <Each />
 
