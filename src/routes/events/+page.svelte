185ce313ee51ebab84065cda5c607aee8e784d0a
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-26 09:21:09 -0400

    Create component event dispatcher

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,16 +1,26 @@
 <script>
-	let m = { x: 0, y: 0 };
+	// let m = { x: 0, y: 0 };
+
+    import Inner from "../../components/Inner.svelte";
 
 	// function handleMove(event) {
 	// 	m.x = event.clientX;
 	// 	m.y = event.clientY;
 	// }
+
+    function handleMessage(event) {
+		alert(event.detail.text);
+	}
 </script>
 
 <h1>Events Examples</h1>
 <button on:click|once={() => alert('clicked')}>
 	Click me
 </button>
+
+<div>
+    <Inner on:message={handleMessage} />
+</div>
 
 <!-- <div 
     on:pointermove={(e) => {
@@ -22,12 +32,16 @@
 </div> -->
 
 <style>
-	.pointer {
+    div, button {
+        margin-bottom: 1rem;
+    }
+
+	/* .pointer {
 		position: fixed;
 		left: 0;
 		top: 0;
 		width: 100%;
 		height: 100%;
 		padding: 1rem;
-	}
+	} */
 </style>
