785297a2ac75719805e81695ff461b3d7fcfd02d
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-27 06:30:24 -0400

    Add text, number, and range input bindings

---
+++ src/routes/bindings/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let name = 'world';
+
+    let a = 1;
+    let b = 2;
+</script>
+
+<h1>Bindings Examples</h1>
+
+<input bind:value={name} />
+
+<h1>Hello {name}!</h1>
+
+<label>
+	<input type="number" bind:value={a} min="0" max="10" />
+	<input type="range" bind:value={a} min="0" max="10" />
+</label>
+
+<label>
+	<input type="number" bind:value={b} min="0" max="10" />
+	<input type="range" bind:value={b} min="0" max="10" />
+</label>
+
+<p>{a} + {b} = {a + b}</p>
