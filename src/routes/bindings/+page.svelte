24de33036e837b4a0f19f6de303f7e118f854994
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-29 07:10:27 -0400

    Add select binding example

--- src/routes/bindings/+page.svelte
+++ src/routes/bindings/+page.svelte
@@ -5,6 +5,31 @@
     let b = 2;
 
     let yes = false;
+
+    let questions = [
+		{
+			id: 1,
+			text: `Where did you go to school?`
+		},
+		{
+			id: 2,
+			text: `What is your mother's name?`
+		},
+		{
+			id: 3,
+			text: `What is another personal fact that an attacker could easily find with Google?`
+		}
+	];
+
+	let selected;
+
+	let answer = '';
+
+	function handleSubmit() {
+		alert(
+			`answered question ${selected.id} (${selected.text}) with "${answer}"`
+		);
+	}
 </script>
 
 <h1>Bindings Examples</h1>
@@ -49,6 +74,35 @@
 {/if}
 
 <button disabled={!yes}>Subscribe</button>
+
+<h2>Example 4</h2>
+
+<h3>Insecurity questions</h3>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<select
+		bind:value={selected}
+		on:change={() => (answer = '')}
+	>
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
+
+	<input bind:value={answer} />
+
+	<button disabled={!answer} type="submit">
+		Submit
+	</button>
+</form>
+
+<p>
+	selected question {selected
+		? selected.id
+		: '[waiting...]'}
+</p>
 
 <style>
     h2 {
