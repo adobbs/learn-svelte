0ab9a5a059a7b51b17120f953f689347b76ed8b8
Andy Dobbs <andrew.dobbs@gmail.com>
2023-06-04 05:55:23 -0500

    Add writeable store example

---
+++ src/routes/stores/writeable/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Decrementer from '../../../components/stores/Decrementer.svelte';
+    import Incrementer from '../../../components/stores/Incrementer.svelte';
+    import Resetter from '../../../components/stores/Resetter.svelte';
+    import { count } from '../../../components/stores/stores.js';
+
+	let count_value;
+
+	count.subscribe((value) => {
+		count_value = value;
+	});
+</script>
+
+<h1>The count is {count_value}</h1>
+
+<Incrementer />
+<Decrementer />
+<Resetter />
