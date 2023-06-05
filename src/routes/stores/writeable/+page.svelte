046f882bc10a0ecf541037600bde45cf48018c5e
Andy Dobbs <andrew.dobbs@gmail.com>
2023-06-05 06:14:46 -0500

    Auto subscribe to a writeable store

--- src/routes/stores/writeable/+page.svelte
+++ src/routes/stores/writeable/+page.svelte
@@ -3,15 +3,9 @@
     import Incrementer from '../../../components/stores/Incrementer.svelte';
     import Resetter from '../../../components/stores/Resetter.svelte';
     import { count } from '../../../components/stores/stores.js';
-
-	let count_value;
-
-	count.subscribe((value) => {
-		count_value = value;
-	});
 </script>
 
-<h1>The count is {count_value}</h1>
+<h1>The count is {$count}</h1>
 
 <Incrementer />
 <Decrementer />
