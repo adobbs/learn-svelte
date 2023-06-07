4b752989167e3f202373aa8db7f9aa1e622a3602
Andy Dobbs <andrew.dobbs@gmail.com>
2023-06-06 22:40:32 -0400

    Add readable store example

--- src/components/stores/stores.js
+++ src/components/stores/stores.js
@@ -1,3 +1,19 @@
+// Writeable
+
 import { writable } from 'svelte/store';
 
 export const count = writable(0);
+
+// Readable
+
+import { readable } from 'svelte/store';
+
+export const time = readable(new Date(), function start(set) {
+	const interval = setInterval(() => {
+		set(new Date());
+	}, 1000);
+
+	return function stop() {
+		clearInterval(interval);
+	};
+});
